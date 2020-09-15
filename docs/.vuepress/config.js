ed99864b8c9412069794104bf6eb7ee9c137321d
henryfordstick <henryfordstick@gmail.com>
2020-09-15 15:12:59 +0800

    代码更新

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,92 @@
+module.exports = {
+  title: "渔网的收获",
+  description: "",
+  markdown: {
+    lineNumbers: true
+  },
+  head: [
+    ['link', { rel: 'icon', href: '/user.png' }]
+  ],
+  themeConfig:{
+    repo: 'henryfordstick',
+    nav: [
+      {
+        text: "博客",
+        link: "/blog/",
+      },
+      {
+        text: "算法",
+        link: "/arith/",
+      },
+      // {
+      //   text: "面试题",
+      //   link: "/interview/",
+      // },
+      {
+        text: "阅读",
+        link: "/book/",
+      },
+    ],
+    sidebar: {
+      '/blog/':[
+        {
+          title: '视野拓展',
+          collapsable: false,
+        },
+        {
+          title: 'Framework',
+          collapsable: false,
+        },
+        {
+          title: 'HTML/CSS',
+          collapsable: false,
+        },
+        {
+          title: 'JavaScript基础',
+          collapsable: false,
+        },
+        {
+          title: '工程化',
+          collapsable: false,
+        },
+        {
+          title: '编程基础',
+          collapsable: false,
+        },
+        // {
+        //   title: '总结思考',
+        //   collapsable: false,
+        // }
+      ],
+      '/arith/':[
+        {
+          title: "算法知识",
+          collapsable: false,
+          children: [
+            'arith-basic'
+          ]
+        },
+        {
+          title: "经典算法题",
+          collapsable: false,
+        }
+      ],
+      // '/interview/':[],
+      '/book/':[
+        {
+          title: "技术相关",
+          collapsable: false,
+        },
+        {
+          title: "其他",
+          collapsable: false,
+        }
+      ]
+    },
+    lastUpdated: "更新时间",
+    docsDir: "docs",
+  },
+  plugins: [
+
+  ]
+};
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: /user.png
+tagline: 
+actionText: 开始阅读 →
+actionLink: /blog/
+features:
+  - title: 积累
+    details: 平时的一些积累，一些上手即用的东西/知识点等内容，范围较广。
+  - title: 面试题
+    details: 自己经历的面试题，或者遇到的比较有趣的面试题。
+  - title: 持续更新
+    details: 本文档将会持续更新，当做一个长期项目来维护。
+footer: MIT Licensed | Copyright © 2018-present henry
+---
